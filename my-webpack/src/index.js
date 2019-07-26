fee28f0d96a55bff46543f386779323b8299f622
tanxingwen <tanxingwen@hifive.ai>
2019-07-26 17:34:46 +0800

    update

---
+++ my-webpack/src/index.js
@@ -0,0 +1,5 @@
+import sum from './common/sum'
+
+export default function (a = 0, b = 0) {
+  return sum(a, b)
+}
---
+++ my-webpack/webpack.config.js
@@ -0,0 +1,14 @@
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
